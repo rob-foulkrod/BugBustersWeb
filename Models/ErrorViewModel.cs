14606ad7945db243bb43bf396c1ea93486a0f33e
Rob Foulkrod <robfoulkrod@microsoft.com>
2024-10-03 13:31:26 -0400

    Created initial base web project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BugBustersWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
